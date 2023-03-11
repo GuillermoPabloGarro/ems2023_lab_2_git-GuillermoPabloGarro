ff6e62263dd6258444a8b285ef6d973eb0bdc6a6
GuillermoPabloGarro <guillermo.pablo.garro@alumnos.upm.es>
2023-03-11 19:16:33 +0100

    add web greeting controller issue #X1

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,16 +1,15 @@
 package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.getMapping;
-import org.springframework.web.bind.annotation.requestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required = false, defaultValue="World")
-    String name, Model model) {
-        model.addAtribute("name", name);
-        return "greeting";
-    }
+ 	@GetMapping("/greeting")
+ 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World")
+String name, Model model) {
+ 	model.addAttribute("name", name);
+ 	return "greeting";
+ }
 }
